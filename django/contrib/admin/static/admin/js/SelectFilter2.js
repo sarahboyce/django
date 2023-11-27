sarahboyce/django

commit 1699f8b52ac15f687cc39088401c2641022c71cd

Fixed #34628 -- Improved icons accessibility of related widget wrapper in admin.
This improves accessibility of icons by making them larger and grayscale
(when not selected).

django/contrib/admin/static/admin/css/widgets.css
@@ -579,16 +579,13 @@ ul.timelist, .timelist li {
 }
 
 .related-widget-wrapper-link {
-    opacity: 0.3;
+    opacity: .6;
+    filter: grayscale(1);
 }
 
 .related-widget-wrapper-link:link {
-    opacity: .8;
-}
-
-.related-widget-wrapper-link:link:focus,
-.related-widget-wrapper-link:link:hover {
     opacity: 1;
+    filter: grayscale(0);
 }
 
 select + .related-widget-wrapper-link,

django/contrib/admin/templates/admin/widgets/related_widget_wrapper.html
@@ -9,7 +9,7 @@
             data-href-template="{{ change_related_template_url }}?{{ url_params }}"
             data-popup="yes"
             title="{% blocktranslate %}Change selected {{ model }}{% endblocktranslate %}">
-            <img src="{% static 'admin/img/icon-changelink.svg' %}" alt="">
+            <img src="{% static 'admin/img/icon-changelink.svg' %}" alt="" width="20" height="20">
         </a>
         {% endif %}
         {% if can_add_related %}
@@ -17,7 +17,7 @@
             data-popup="yes"
             href="{{ add_related_url }}?{{ url_params }}"
             title="{% blocktranslate %}Add another {{ model }}{% endblocktranslate %}">
-            <img src="{% static 'admin/img/icon-addlink.svg' %}" alt="">
+            <img src="{% static 'admin/img/icon-addlink.svg' %}" alt="" width="20" height="20">
         </a>
         {% endif %}
         {% if can_delete_related %}
@@ -25,14 +25,14 @@
             data-href-template="{{ delete_related_template_url }}?{{ url_params }}"
             data-popup="yes"
             title="{% blocktranslate %}Delete selected {{ model }}{% endblocktranslate %}">
-            <img src="{% static 'admin/img/icon-deletelink.svg' %}" alt="">
+            <img src="{% static 'admin/img/icon-deletelink.svg' %}" alt="" width="20" height="20">
         </a>
         {% endif %}
         {% if can_view_related %}
           <a class="related-widget-wrapper-link view-related" id="view_id_{{ name }}"
              data-href-template="{{ change_related_template_url }}?{{ view_related_url_params }}"
              title="{% blocktranslate %}View selected {{ model }}{% endblocktranslate %}">
-            <img src="{% static 'admin/img/icon-viewlink.svg' %}" alt="">
+            <img src="{% static 'admin/img/icon-viewlink.svg' %}" alt="" width="20" height="20">
           </a>
         {% endif %}
         {% endif %}

tests/admin_views/test_related_object_lookups.py
@@ -16,7 +16,7 @@ class SeleniumTests(AdminSeleniumTestCase):
             username="super", password="secret", login_url=reverse("admin:index")
         )
 
-    def test_related_object_link_images_empty_alt(self):
+    def test_related_object_link_images_attributes(self):
         from selenium.webdriver.common.by import By
 
         album_add_url = reverse("admin:admin_views_album_add")
@@ -34,6 +34,8 @@ class SeleniumTests(AdminSeleniumTestCase):
                     By.XPATH, f'//*[@id="{link_id}"]/img'
                 )
                 self.assertEqual(link_image.get_attribute("alt"), "")
+                self.assertEqual(link_image.get_attribute("width"), "20")
+                self.assertEqual(link_image.get_attribute("height"), "20")
 
     def test_related_object_lookup_link_initial_state(self):
         from selenium.webdriver.common.by import By

commit 089bccf1453bbcbcc58b16ad777d1e5576c6a347

Fixed #34995 -- Fixed position of related widget's add link on admin pages for smaller screens.
Regression in 1699f8b52ac15f687cc39088401c2641022c71cd.

Co-authored-by: Sarah Boyce <42296566+sarahboyce@users.noreply.github.com>

django/contrib/admin/static/admin/css/responsive.css
@@ -237,22 +237,6 @@ input[type="submit"], button {
         padding: 7px;
     }
 
-    /* Related widget */
-
-    .related-widget-wrapper {
-        float: none;
-    }
-
-    .related-widget-wrapper-link + .selector {
-        max-width: calc(100% - 30px);
-        margin-right: 15px;
-    }
-
-    select + .related-widget-wrapper-link,
-    .related-widget-wrapper-link + .related-widget-wrapper-link {
-        margin-left: 10px;
-    }
-
     /* Selector */
 
     .selector {
@@ -694,10 +678,6 @@ input[type="submit"], button {
         flex-direction: column;
     }
 
-    .selector > * {
-        float: none;
-    }
-
     .selector-available, .selector-chosen {
         margin-bottom: 0;
         flex: 1 1 auto;
@@ -709,7 +689,6 @@ input[type="submit"], button {
 
     .selector ul.selector-chooser {
         display: block;
-        float: none;
         width: 52px;
         height: 26px;
         padding: 0 2px;

django/contrib/admin/static/admin/css/widgets.css
@@ -574,7 +574,8 @@ ul.timelist, .timelist li {
 
 /* RELATED WIDGET WRAPPER */
 .related-widget-wrapper {
-    float: left;       /* display properly in form rows with multiple fields */
+    display: flex;
+    gap: 6px;
     overflow: hidden;  /* clear floated contents */
 }
 

django/contrib/admin/static/admin/js/SelectFilter2.js
@@ -30,6 +30,9 @@ Requires core.js and SelectBox.js.
 
             // <div class="selector"> or <div class="selector stacked">
             const selector_div = quickElement('div', from_box.parentNode);
+            // Make sure the selector div is at the beginning so that the
+            // add link would be displayed to the right of the widget.
+            from_box.parentNode.prepend(selector_div);
             selector_div.className = is_stacked ? 'selector stacked' : 'selector';
 
             // <div class="selector-available">

js_tests/admin/SelectFilter2.test.js
@@ -5,9 +5,13 @@ QUnit.module('admin.SelectFilter2');
 
 QUnit.test('init', function(assert) {
     const $ = django.jQuery;
-    $('<form><select id="id"></select></form>').appendTo('#qunit-fixture');
-    $('<option value="0">A</option>').appendTo('#id');
+    $('<form id="test"></form>').appendTo('#qunit-fixture');
+    $('<label for="id_id">Test</label>').appendTo('#test');
+    $('<div class="helptext">This is helpful.</div>').appendTo('#test');
+    $('<select id="id"><option value="0">A</option></select>').appendTo('#test');
     SelectFilter.init('id', 'things', 0);
+    assert.equal($('#test').children().first().prop("tagName"), "DIV");
+    assert.equal($('#test').children().first().attr("class"), "selector");
     assert.equal($('.selector-available h2').text().trim(), "Available things");
     assert.equal($('.selector-chosen h2').text().trim(), "Chosen things");
     assert.equal($('.selector-chosen select')[0].getAttribute('multiple'), '');